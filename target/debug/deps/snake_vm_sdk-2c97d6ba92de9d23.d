7f336261b99f4ed74e98bb26a7615ccfd4f99210
Nathan Fox <fuchsnj@gmail.com>
2021-12-15 23:04:13 -0500

    fix .gitignore

--- target/debug/deps/snake_vm_sdk-2c97d6ba92de9d23.d
+++
@@ -1,7 +0,0 @@
-/home/nathan/repos/snake_vm_sdk/target/debug/deps/snake_vm_sdk-2c97d6ba92de9d23: src/lib.rs src/raw.rs src/sdk.rs
-
-/home/nathan/repos/snake_vm_sdk/target/debug/deps/snake_vm_sdk-2c97d6ba92de9d23.d: src/lib.rs src/raw.rs src/sdk.rs
-
-src/lib.rs:
-src/raw.rs:
-src/sdk.rs:
--- target/debug/deps/snake_vm_sdk-d29b4b77ef543c5a.d
+++
@@ -1,7 +0,0 @@
-/home/nathan/repos/snake_vm_sdk/target/debug/deps/snake_vm_sdk-d29b4b77ef543c5a.rmeta: src/lib.rs src/raw.rs src/sdk.rs
-
-/home/nathan/repos/snake_vm_sdk/target/debug/deps/snake_vm_sdk-d29b4b77ef543c5a.d: src/lib.rs src/raw.rs src/sdk.rs
-
-src/lib.rs:
-src/raw.rs:
-src/sdk.rs:
