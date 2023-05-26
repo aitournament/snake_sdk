81f4ef01b931ddc5e5f5ea195d403612b50786fa
Nathan Fox <fuchsnj@gmail.com>
2023-05-25 22:47:07 -0400

    remove unused Observation::DeadSnake

--- src/sdk.rs
+++ src/sdk.rs
@@ -1,5 +1,5 @@
 use crate::raw;
-use crate::raw::{DIRECTION_NORTH, DIRECTION_EAST, DIRECTION_WEST, DIRECTION_SOUTH};
+use crate::raw::{DIRECTION_EAST, DIRECTION_NORTH, DIRECTION_SOUTH, DIRECTION_WEST};
 use core::hint::unreachable_unchecked;
 use core::mem::MaybeUninit;
 
@@ -17,9 +17,6 @@ pub enum Observation {
     Food(FoodInfo),
     SnakeHead(SnakeInfo),
     SnakeBody(SnakeInfo),
-
-    /// A dead snake, with the tick it will turn into food
-    DeadSnake(u32),
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -39,7 +36,9 @@ pub fn get_cpu_cycles_per_tick() -> u32 {
 }
 
 pub fn set_direction(direction: Direction) {
-    unsafe { raw::set_direction(direction as u32); }
+    unsafe {
+        raw::set_direction(direction as u32);
+    }
 }
 
 /// Move immediately (at the end of the current CPU cycle) in the current direction. A snake must move once per tick.
@@ -57,11 +56,15 @@ pub fn move_snake() {
 /// Performing a leap will consume the last body part of this snake, which will drop on the floor as food.
 /// If this is incorrectly called, the snake will die from exhaustion.
 pub fn leap() {
-    unsafe { raw::leap(); }
+    unsafe {
+        raw::leap();
+    }
 }
 
 pub fn sleep_remaining_tick() {
-    unsafe { raw::sleep_remaining_tick(); }
+    unsafe {
+        raw::sleep_remaining_tick();
+    }
 }
 
 pub fn sleep(cycles: u64) {
@@ -89,7 +92,6 @@ pub fn get_current_pos() -> (u32, u32) {
     }
 }
 
-
 pub fn get_current_tick() -> u64 {
     unsafe { raw::get_current_tick() }
 }
@@ -102,7 +104,6 @@ pub fn suicide() {
     unsafe { raw::suicide() }
 }
 
-
 pub fn split() {
     unsafe { raw::split() }
 }
@@ -113,12 +114,19 @@ pub fn observe(x: u32, y: u32) -> Observation {
     let mut out_1 = MaybeUninit::<u32>::uninit();
     let mut out_2 = MaybeUninit::<u32>::uninit();
     unsafe {
-        raw::observe(x, y, type_out.as_mut_ptr(), out_0.as_mut_ptr(), out_1.as_mut_ptr(), out_2.as_mut_ptr());
+        raw::observe(
+            x,
+            y,
+            type_out.as_mut_ptr(),
+            out_0.as_mut_ptr(),
+            out_1.as_mut_ptr(),
+            out_2.as_mut_ptr(),
+        );
     }
     match unsafe { type_out.assume_init() } {
         raw::TYPE_EMPTY => Observation::Empty,
-        raw::TYPE_FOOD => Observation::Food(FoodInfo{
-            health_value: unsafe {out_0.assume_init() as i32}
+        raw::TYPE_FOOD => Observation::Food(FoodInfo {
+            health_value: unsafe { out_0.assume_init() as i32 },
         }),
         raw::TYPE_SNAKE_HEAD => Observation::SnakeHead(SnakeInfo {
             owner_id: unsafe { out_0.assume_init() },
@@ -130,7 +138,7 @@ pub fn observe(x: u32, y: u32) -> Observation {
             snake_id: unsafe { out_1.assume_init() },
             health: unsafe { out_2.assume_init() },
         }),
-        _ => unsafe { unreachable_unchecked() }
+        _ => unsafe { unreachable_unchecked() },
     }
 }
 
